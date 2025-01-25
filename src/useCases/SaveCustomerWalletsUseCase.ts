342bdea30bbfb42dff7f9787f8b12efb550539dd
darlan raimundo <darlanraimundo123@gmail.com>
2025-01-25 14:40:31 -0300

    implementação rota para resgate do cliente por id

--- src/useCases/SaveCustomerWalletsUseCase.ts
+++ src/useCases/SaveCustomerWalletsUseCase.ts
@@ -35,9 +35,9 @@ export default class SaveCustomerWalletsUseCase
     );
 
     if (!customers) {
-      throw new Error("Erro ao salvar cliente na base.");
+      throw new Error("Erro ao salvar clientes na base.");
     }
 
-    return { message: "Cliente salvo com sucesso." };
+    return { message: "Clientes salvo com sucesso." };
   }
 }
